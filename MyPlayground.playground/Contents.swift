20d5b13960f71399b412e6a0c426f5ccda5d087a
Apple <apple@Apples-MBP-4.attlocal.net>
2019-07-09 10:17:38 -0700

    Intial commit.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+5<3
+13>7
+6 != 8
+//7 == 7
+"karlie" == "karlie"
+"karlie" == " karlie"
+"Karlie" == "karlie"
+
+
+var dogAge = 2
+if dogAge < 2{
+    print("you are a puppy")
+}else if dogAge > 12{
+    print("you are not a puppy")
+}else{
+    print("you are also awesome")
+}
+
+var favFood = "ShareTea"
+if favFood == "Chipotle"{
+    print("gross")
+}else if favFood == "Starbucks"{
+    print("close")
+}else{
+    print("awesome")
+}
+
+var hasAccount = false
+
+if hasAccount == true {
+    print("please login")
+}else{
+    print("get more info for account")
+}
+
+var gradeLevel = 12
+
+if gradeLevel <= 5 {
+    print("you are in elementary")
+}else if gradeLevel <= 8 {
+    print("you are in middleschool")
+}else if gradeLevel  <= 12{
+    print("you are in highschool")
+}else if gradeLevel == 0 {
+    print("you are in preschool")
+}else{
+    print("you are not in school")
+}
---
+++ dataTypesPractice.playground/Contents.swift
@@ -0,0 +1,30 @@
+//import UIKit
+//var greeting = "Hello, world"
+//print(greeting)
+//greeting = "hello, karlie"
+//print(greeting)
+//var name = "Eric"
+//print(name)
+////hi audrey:)
+//var newGreeting = "Hello, \(name)"
+//print(newGreeting)
+//name = "aarushi"
+//print(*\(new))
+
+
+
+
+
+
+
+
+
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+
+
+var average = Double(a+b+c+d)/4
+print(average)
+
